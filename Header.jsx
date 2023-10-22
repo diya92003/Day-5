2b1057925f29d03bec106957748d6552211ef84b
diya92003 <147637741+diya92003@users.noreply.github.com>
2023-10-22 15:15:06 +0530

    Header.jsx

---
+++ Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+function Header(){
+    return (
+    <div id="box">
+        <h1>Mahatma Gandhi</h1>
+        <img id="image" src="https://cdn.britannica.com/91/82291-050-EB7A276A/Mohandas-K-Gandhi-leader-Mahatma-Indian.jpg?w=400&h=300&c=crop" alt="" />
+    </div>
+    )
+}
+export default Header;
