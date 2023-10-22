2de487df9dd4b901f607df1b37edbf6ae8fbedb0
diya92003 <147637741+diya92003@users.noreply.github.com>
2023-10-22 15:18:46 +0530

    Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,18 @@
+import React,{Component} from "react";
+class Footer extends Component {
+    render() {
+        return (
+            <>
+            <hr />
+            <h3>Contact</h3>
+            <p>
+                <b>Mobile: </b>987654321
+            </p>
+            <p>
+                <b>Email: </b>bleh@gmail.com
+            </p>
+            </>
+        );
+    }
+}
+export default Footer;
